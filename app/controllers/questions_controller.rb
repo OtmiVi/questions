601b432cde98f7310a17a4969400a259d8eb546f
Yura Kyziv <y.kuziv@apzs.eu>
2025-02-17 23:53:06 +0200

    create questions crud

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,42 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new questions_params
+
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+  def edit
+    @question = Question.find(params[:id])
+  end
+  def update
+    @question = Question.find(params[:id])
+    if @question.update questions_params
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def questions_params
+    params.require(:question).permit(:title, :body)
+  end
 end
