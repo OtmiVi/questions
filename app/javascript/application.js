a93e51590e357b9092ff779333bb60ab14ffe481
Yura Kyziv <y.kuziv@apzs.eu>
2025-02-19 00:29:06 +0200

    Add custom styles

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Entry point for the build script in your package.json
 import Rails from "@rails/ujs";
+//import * as bootstrap from "bootstrap"
 Rails.start();
-import * as bootstrap from "bootstrap"
+
