31e870f2c316f77efe64c1a687bfe1003f1995b3
Yura Kyziv <y.kuziv@apzs.eu>
2025-02-19 23:46:44 +0200

    Create answer table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_14_190304) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_19_201835) do
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.integer "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
   create_table "questions", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "answers", "questions"
 end
