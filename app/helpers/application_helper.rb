95855dd8df2ac0e149d712aea65bc8a54bb6ff7d
Yura Kyziv <y.kuziv@apzs.eu>
2025-02-26 19:06:57 +0200

    Add menu helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+  def nav_link(link_text, link_path)
+    class_name = @current_page == link_text  ? "nav-link active" : "nav-link"
+    content_tag(:li, class: "nav-item") do
+      link_to link_text, link_path, class: class_name
+    end
+  end
+  def set_current_page(title)
+    @current_page = title
+  end
 end
